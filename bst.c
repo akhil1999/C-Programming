9fcd5a01ba4900f156b373be8c4d42837ff471e1
akhil1999 <akhileshbhuvanendra@gmail.com>
2024-01-29 12:32:26 +0530

    Add Binary Search Tree code

---
+++ bst.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct bstNode{
+	int data;
+	struct bstNode* left;
+	struct bstNode* right;
+};
+
+int timesCalled = 0;
+int numberOfNodes = 0;
+
+struct bstNode* root;
+
+struct bstNode* getNewNode(int data){
+	struct bstNode* newNode = (struct bstNode*) malloc(sizeof(struct bstNode));
+	newNode->data = data;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	return newNode;
+}
+
+struct bstNode* Insert(struct bstNode* root, int data){
+	//tree empty, so create a new tree
+	if(root == NULL){
+		root = getNewNode(data);
+	}else if(data <= root->data){
+		root->left = Insert(root->left,data);
+	}else{
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+
+bool bstSearch(struct bstNode* root, int data){
+	if(root == NULL){
+		return false;
+	}else if(root->data == data){
+		return true;
+	}else if(data <= root->data){
+		return bstSearch(root->left,data);
+	}else{
+		return bstSearch(root->right, data);
+	}
+}
+
+int findMin(struct bstNode* root){
+	int min = 0;
+	if(root->left != NULL){
+		min = findMin(root->left);
+	}else{
+		return root->data;
+	}
+}
+
+int findMax(struct bstNode* root){
+	int max = 0;
+	if(root->right != NULL){
+		max = findMax(root->right);
+	}else{
+		return root->data;
+	}
+}
+
+int max(int a, int b){
+	if(a > b){
+		return a;
+	}else if(a < b){
+		return b;
+	}else{
+		return a;
+	}
+}
+
+int findHeight(struct bstNode* root){
+	timesCalled++;
+	//return -1 if only one node detected / last node
+	if(root == NULL) {
+		return -1;
+	}
+	return max(findHeight(root->left), findHeight(root->right)) + 1;
+}
+
+void findNodes(struct bstNode* root){
+	if(root == NULL){
+		return;
+	}else{
+		numberOfNodes++;
+	}
+	findNodes(root->left);
+	findNodes(root->right);
+}
+
+
+void main(){
+	root = NULL;
+	root = Insert(root, 15);
+	root = Insert(root, 10);
+	root = Insert(root, 20);
+	root = Insert(root, 25);
+	root = Insert(root, 8);
+	root = Insert(root, 12);
+	root = Insert(root, 1);
+	root = Insert(root, -69);
+	root = Insert(root, 22);
+	root = Insert(root, 23);
+/*	int number_to_be_searched = 0;*/
+/*	printf("Enter number to search:\n");*/
+/*	scanf("%d", &number_to_be_searched);*/
+/*	if(bstSearch(root,number_to_be_searched) == true) {*/
+/*		printf("found!");*/
+/*	}else{*/
+/*		printf("rip");*/
+/*	}*/
+	printf("min:%d\n", findMin(root));
+	printf("max:%d\n", findMax(root));
+	printf("height:%d\n", findHeight(root));
+	printf("times findHeight called:%d\n", timesCalled);
+	findNodes(root);
+	printf("number of nodes:%d\n", numberOfNodes);
+}
