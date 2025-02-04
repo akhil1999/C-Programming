c8c049364a1cd1789d490fe651a55ddc5b774187
akhil1999 <akhileshbhuvanendra@gmail.com>
2025-02-04 12:52:24 +0530

    Add finding first and last occurrence of an element using binary search code

---
+++ first_last_occurence.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int binarySearchFirstOccurence(int *array, int len, int target){
+	int low = 0;
+	int high = len - 1;
+	int result = -1;
+	while(low<=high){
+		int mid = low + (high - low) / 2;
+		if(array[mid] == target){
+			result = mid;
+			high = mid - 1;
+		}
+		else if(array[mid] > target) high = mid - 1;
+		else low = mid + 1;
+	}
+	return result;
+}
+
+int binarySearchLastOccurence(int *array, int len, int target){
+	int low = 0;
+	int high = len - 1;
+	int result = -1;
+	while(low<=high){
+		int mid = low + (high - low) / 2;
+		if(array[mid] == target){
+			result = mid;
+			low = mid + 1;
+		}
+		else if(array[mid] > target) high = mid - 1;
+		else low = mid + 1;
+	}
+	return result;
+}
+
+void main(){
+	int a[10] = {0,2,3,4,4,4,90,101, 200, 300};
+	int *end = (a+9);
+	printf("Enter element to search:");
+	int target = 0;
+	scanf("%d", &target);
+	printf("First Occurrence:%d\n", binarySearchFirstOccurence(a, 10, target));
+	printf("Last Occurrence:%d\n", binarySearchLastOccurence(a, 10, target));
+}
